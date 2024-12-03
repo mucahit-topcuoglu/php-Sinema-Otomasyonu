1162fdf3acb078e87d8c50289a46cb38731b9998
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:36:00 +0300

    cikis.php
    Oturum Başlatma
    session_start(); komutu, PHP oturumunu başlatır. Bu, oturum verilerini (örneğin, kullanıcıya ait bilgiler) kullanabilmek için gereklidir. Oturum, kullanıcıya ait bilgilerin sunucu tarafında saklanmasını sağlar.
    
    Oturum Değişkenlerini Temizleme
    unset() fonksiyonu, belirtilen oturum değişkenini bellekten kaldırır. Bu sayede kullanıcı bilgileri temizlenir, yani oturumdaki kullanıcı bilgileri sıfırlanır. Burada oturumdan sırasıyla isim, id, kadi, eposta ve telefon bilgileri silinmiştir.
    
    Yönlendirme
    header("Refresh: 0; url=index.php"); komutu, sayfa yenilendikten sonra kullanıcıyı index.php sayfasına yönlendirir. Bu, oturumdan çıkışı tamamlayan ve kullanıcıyı giriş sayfasına gönderen işlemi yapar.
    
    Bu Kodun Amacı:
    Bu PHP kodu, kullanıcıyı oturumdan çıkartmak için kullanılır. Kullanıcı çıkış yaptıktan sonra, oturumdaki tüm kişisel veriler temizlenir ve kullanıcı ana sayfaya (index.php) yönlendirilir. Bu tür bir işlem, genellikle güvenlik nedeniyle kullanıcıların çıkış yapmalarını sağlamak için kullanılır.

--- Sinema Otomasyon/cikis.php
+++ Sinema Otomasyon/cikis.php
@@ -1,9 +1,11 @@
 <?php
-session_start();
-unset($_SESSION["isim"]);
-unset($_SESSION["id"]);
-unset($_SESSION["kadi"]);
-unset($_SESSION["eposta"]);
-unset($_SESSION["telefon"]);
-header("Refresh: 0; url=index.php");
- ?>
+session_start(); // PHP oturumunu başlatır. Bu, oturum verilerini kullanabilmek için gereklidir.
+
+unset($_SESSION["isim"]); // 'isim' adlı oturum değişkenini temizler (kaldırır).
+unset($_SESSION["id"]); // 'id' adlı oturum değişkenini temizler (kaldırır).
+unset($_SESSION["kadi"]); // 'kadi' (kullanıcı adı) adlı oturum değişkenini temizler.
+unset($_SESSION["eposta"]); // 'eposta' adlı oturum değişkenini temizler.
+unset($_SESSION["telefon"]); // 'telefon' adlı oturum değişkenini temizler.
+
+header("Refresh: 0; url=index.php"); // Sayfa yüklemesinden hemen sonra 'index.php' sayfasına yönlendirir. Bu, çıkış işleminden sonra kullanıcıyı giriş sayfasına yönlendirmeyi sağlar.
+?>
