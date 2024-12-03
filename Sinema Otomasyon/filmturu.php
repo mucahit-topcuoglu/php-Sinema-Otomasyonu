02e97a1192fe530b244f585420b122897f81b3aa
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:43:30 +0300

    filmturu.php
    Header ve Menü:
    
    Menüdeki her bir öğe, yönetim paneline veya sayfalarına yönlendiren bağlantıları içeriyor.
    filmler.php, seansislemleri.php gibi sayfalara yönlendiren bağlantılar bulunuyor.
    Film Türü Ekleme Formu:
    
    Kullanıcıdan "Film Türü" bilgisini almak için input elemanı var. Bu form, veriyi islemler.php dosyasına gönderiyor ve orada işleniyor.
    Film türü eklemek için bir buton var.
    Film Türlerini Görüntüleme:
    
    Kullanıcı "Film Türlerini Görüntüle" butonuna tıkladığında, filmturugoruntule form parametresi gönderiliyor ve film türlerini veritabanından listeleyen bir sorgu çalıştırılıyor.
    Sorgudan dönen veriler tablo şeklinde görüntüleniyor.
    Film Türünü Silme:
    
    "Film Türünü Sil" butonu, kullanıcının bir film türünü silmesini sağlamak için filmturusil.php dosyasına yönlendiren bir bağlantıdır.
    PHP Kodları:
    
    isset($_POST["filmturugoruntule"]) ile buton tıklanıp tıklanmadığı kontrol ediliyor.
    mysqli_query($baglan, "SELECT * FROM filmturleri") komutu ile veritabanından tüm film türleri alınır.
    while ($satir = mysqli_fetch_array($sorgu)) döngüsü ile her bir film türü bir tablo satırında görüntülenir.
    Bu şekilde her satırı açıklamış oldum. Kodun işleyişi hakkında her adımı detaylıca açıklamaya çalıştım.

--- Sinema Otomasyon/filmturu.php
+++ Sinema Otomasyon/filmturu.php
@@ -1,56 +1,82 @@
-<?php include ("baglan.php") ?>
+<?php include("baglan.php"); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Sinema Otomasyonu</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width-device-width,intital-scale-1.0">
-	<link rel="stylesheet" href="css/style.css">
+    <title>Sinema Otomasyonu</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<header>
-		<h2><ul>
-		<li><a href="filmler.php">Film İşlemleri</a></li>
-		<li><a href="seansislemleri.php">Seans İşlemleri</a></li>
-		<li><a href="filmturu.php">Film Türü</a></li>
-		<li><a href="biletislemleri.php">Bilet İşlemleri</a></li>
-		<li><a href="musteriler.php">Müşteriler</a></li>
-		<li><a href="cikis.php">Çıkış</a></li>
-		</ul></h2>
-	</header>
-	<h2>Film Türü Ekle</h1>
-		<div class="tableouterft"><br><br><br><br>
-		<form action="islemler.php" method="post">
-			<input type="text" name="filmturu" placeholder="Film Türü" required="">
-			<button type="submit" class="sub" id="giris" name="filmturuekle">Film Türü Ekle</button>	
-		</form>
-		<form action="" method="post">
-			<button class="sub" type="submit" id="giris" name="filmturugoruntule">Film Türlerini Görüntüle</button>
-		</form>
-		<a href="filmturusil.php"><button type="submit" class="sub" id="uye" name="uye">Film Türünü Sil</button></a>
-				<?php
-				if(isset($_POST["filmturugoruntule"]))
-				{
-				?>
-				<div align="center">
-						<table id="personel">
-							<tr>
-								<th>Film Türü id</th>
-								<th>Film Türü</th>	
-							</tr>
-						<?php
-						$sorgu=mysqli_query($baglan,"select*from filmturleri");
-						while($satir=mysqli_fetch_array($sorgu))
-						{
-						echo "<tr>";
-						echo "<td>".$satir["filmtur_id"]."</td>";
-						echo "<td>".$satir["film_turu"]."</td>";
-						echo "<tr>";
-						}
-					}
-					?>
-						</table>
-				</div>
-		</div><br>
+    <!-- Header kısmı, navigasyon menüsünü içerir -->
+    <header>
+        <h2>
+            <ul>
+                <!-- Film İşlemleri Sayfasına Link -->
+                <li><a href="filmler.php">Film İşlemleri</a></li>
+                <!-- Seans İşlemleri Sayfasına Link -->
+                <li><a href="seansislemleri.php">Seans İşlemleri</a></li>
+                <!-- Film Türü Sayfasına Link -->
+                <li><a href="filmturu.php">Film Türü</a></li>
+                <!-- Bilet İşlemleri Sayfasına Link -->
+                <li><a href="biletislemleri.php">Bilet İşlemleri</a></li>
+                <!-- Müşteriler Sayfasına Link -->
+                <li><a href="musteriler.php">Müşteriler</a></li>
+                <!-- Çıkış Sayfasına Link -->
+                <li><a href="cikis.php">Çıkış</a></li>
+            </ul>
+        </h2>
+    </header>
+
+    <!-- Başlık kısmı, sayfa başlığı -->
+    <h2>Film Türü Ekle</h2>
+    <div class="tableouterft">
+        <br><br><br><br>
+
+        <!-- Film türü eklemek için form -->
+        <form action="islemler.php" method="post">
+            <!-- Kullanıcıdan film türü girmesini isteyen input -->
+            <input type="text" name="filmturu" placeholder="Film Türü" required="">
+            <!-- Film türü ekleme butonu -->
+            <button type="submit" class="sub" id="giris" name="filmturuekle">Film Türü Ekle</button>    
+        </form>
+
+        <!-- Film türlerini görüntülemek için buton -->
+        <form action="" method="post">
+            <button class="sub" type="submit" id="giris" name="filmturugoruntule">Film Türlerini Görüntüle</button>
+        </form>
+
+        <!-- Film türünü silmek için bağlantı -->
+        <a href="filmturusil.php"><button type="submit" class="sub" id="uye" name="uye">Film Türünü Sil</button></a>
+
+        <!-- Film türlerini görüntülemek için şartlı blok -->
+        <?php
+        if (isset($_POST["filmturugoruntule"])) { // Eğer "Film Türlerini Görüntüle" butonuna basılmışsa
+            ?>
+            <div align="center">
+                <!-- Film türlerinin listeleneceği tablo -->
+                <table id="personel">
+                    <tr>
+                        <th>Film Türü id</th> <!-- Film Türü ID başlığı -->
+                        <th>Film Türü</th>    <!-- Film Türü başlığı -->
+                    </tr>
+                    <?php
+                    // Veritabanından tüm film türlerini seçen sorgu
+                    $sorgu = mysqli_query($baglan, "SELECT * FROM filmturleri");
+                    
+                    // Sorgudan dönen her satırı döngü ile al
+                    while ($satir = mysqli_fetch_array($sorgu)) {
+                        echo "<tr>";  // Tablo satırı aç
+                        echo "<td>".$satir["filmtur_id"]."</td>"; // Film türü id'sini tabloya ekle
+                        echo "<td>".$satir["film_turu"]."</td>";  // Film türünü tabloya ekle
+                        echo "</tr>"; // Tablo satırını kapat
+                    }
+                    ?>
+                </table>
+            </div>
+            <?php
+        }
+        ?>
+    </div><br>
 </body>
 </html>
