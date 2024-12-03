6845b33946d65de753c0f399d97fc9b0b95b111b
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:52:36 +0300

    islemler.php
    Kodun Özeti:
    Veritabanı Bağlantısı: baglan.php dosyasını dahil ederek veritabanı bağlantısı kuruyor.
    Müşteri İşlemleri: Müşteri ekleme işlemi musteriekle butonuna tıklanarak yapılır ve veritabanına eklenir.
    Film İşlemleri: Film ekleme işlemi filmekle butonuna tıklanarak yapılır ve filmler tablosuna eklenir.
    Film Türü İşlemleri: Film türü ekleme ve silme işlemleri yapılır.
    Seans İşlemleri: Film, salon ve seans bilgilerini alarak seans ekleme işlemi yapılır.
    Bilet Satışı: Öğrenci ve tam bilet seçeneklerine göre bilet satışı yapılır, biletin fiyatı kullanıcı yaşına göre belirlenir.
    Her işlemde veritabanına veri eklerken veya silerken başarılı olup olmadığını kontrol eder ve kullanıcıya uygun geri bildirimler sağlar.

--- Sinema Otomasyon/islemler.php
+++ Sinema Otomasyon/islemler.php
@@ -1,241 +1,156 @@
-<html>
-<head></head>
-<body>
-
 <?php
-include "baglan.php";
-//---------------------------------------------------------------MÜŞTERİ İŞLEMLERİ ---------------------------------------------------------------------------------->
-	if(isset($_POST["musteriekle"]))
+include "baglan.php"; // Veritabanı bağlantısı için baglan.php dosyasını dahil eder
+?>
+
+<!------------------------------------------------------MÜŞTERİ İŞLEMLERİ---------------------------------------------------------------------------------->
+<?php
+if(isset($_POST["musteriekle"])) // Eğer formda "musteriekle" butonuna tıklanmışsa
 {
-	$musteriekle="insert into musteriler (musteri_adi_soyadi,kadi,sifre,eposta,telefon,yas) VALUES ('" . $_POST["musteri_adi_soyadi"]. "','". $_POST["kadi"]."','". $_POST["sifre"]."','". $_POST["eposta"]."','". $_POST["telefon"]."','". $_POST["yas"]."')";
-	$sonuc=mysqli_query($baglan,$musteriekle);
-	if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png" ></div>';
-				header("Refresh: 1; url=musteriler.php");
-		}
-			else
-		{
-				echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=musteriler.php");
-		}
-	
+    // Müşteri eklemek için SQL sorgusu
+    $musteriekle="insert into musteriler (musteri_adi_soyadi,kadi,sifre,eposta,telefon,yas) VALUES ('" . $_POST["musteri_adi_soyadi"]. "','". $_POST["kadi"]."','". $_POST["sifre"]."','". $_POST["eposta"]."','". $_POST["telefon"]."','". $_POST["yas"]."')";
+    // Sorguyu çalıştır
+    $sonuc=mysqli_query($baglan,$musteriekle);
+    if($sonuc) // Sorgu başarılıysa
+    {
+        echo '<div align="center"><img src="tik.png" ></div>'; // Başarı mesajı
+        header("Refresh: 1; url=musteriler.php"); // 1 saniye sonra müşteri listesine yönlendir
+    }
+    else // Sorgu başarısızsa
+    {
+        echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı
+        header("Refresh: 1; url=musteriler.php"); // 1 saniye sonra müşteri listesine yönlendir
+    }
 }
 ?>
 <!------------------------------------------------------------------------------------------------------------------------------------------------>
 <!------------------------------------------------------------------------------------------------------------------------------------------------->
-<!---------------------------------------------------------------------FİLM İŞLEMLERİ--------------------------------------------------------------->
+<!------------------------------------------------------------FİLM İŞLEMLERİ--------------------------------------------------------------->
 <?php
-	if(isset($_POST["filmekle"]))
-	{
-	$filmekle="insert into filmler (film_adi , filmtur_id) VALUES ('" . $_POST["filmadi"]. "','". 	$_POST["filmturu"]."')";
-	$sonuc=mysqli_query($baglan,$filmekle);
-	if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png" ></div>';
-				header("Refresh: 1; url=filmler.php");
-		}
-			else
-		{
-				echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=filmler.php");
-		}
-	}
-?>
- 
-<!------------------------------------------------------------------------------------------------------------------------------------------------>
-<!------------------------------------------------------------------------------------------------------------------------------------------------->
-<!----------------------------------------------------------------------FİLM TÜRÜ İŞLEMLERİ--------------------------------------------------------->
-<?php
-	if(isset($_POST["filmturuekle"]))
-	{
-			$filmturekle="insert into filmturleri (film_turu) VALUES ('" . $_POST["filmturu"]. "')";
-			$sonuc=mysqli_query($baglan,$filmturekle);
-		if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png" ></div>';
-				header("Refresh: 1; url=filmturu.php");
-		}
-			else
-		{
-					echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=filmturu.php");
-		}
-	}
-?>
-
-<?php
-	if(isset($_POST["filmturusilme"]))
-	{
-		$filmturusil= $_POST['filmturu'];
-		$sorgu="DELETE FROM filmturleri WHERE filmtur_id=$filmturusil";
-		$sonuc=mysqli_query($baglan,$sorgu);
-				if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png" ></div>';
-				header("Refresh: 1; url=filmturusil.php");
-		}
-			else
-		{
-				echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=filmturusil.php");
-		}
-	}
+if(isset($_POST["filmekle"])) // Eğer "filmekle" butonuna tıklanmışsa
+{
+    // Film eklemek için SQL sorgusu
+    $filmekle="insert into filmler (film_adi , filmtur_id) VALUES ('" . $_POST["filmadi"]. "','". 	$_POST["filmturu"]."')";
+    // Sorguyu çalıştır
+    $sonuc=mysqli_query($baglan,$filmekle);
+    if($sonuc) // Sorgu başarılıysa
+    {
+        echo '<div align="center"><img src="tik.png" ></div>'; // Başarı mesajı
+        header("Refresh: 1; url=filmler.php"); // 1 saniye sonra film listesine yönlendir
+    }
+    else // Sorgu başarısızsa
+    {
+        echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı
+        header("Refresh: 1; url=filmler.php"); // 1 saniye sonra film listesine yönlendir
+    }
+}
 ?>
 <!------------------------------------------------------------------------------------------------------------------------------------------------>
 <!------------------------------------------------------------------------------------------------------------------------------------------------->
-<!----------------------------------------------------------------------SEANS İŞLEMLERİ------------------------------------------------------------->
-	<?php
-	if(isset($_POST["seansekle"]))
-	{
-		$seansekle="insert into seanslar (film_id,salon_id,seansSaat_id) VALUES ('"   . $_POST["filmid"]. "','" . $_POST["salonid"]."','" . $_POST["seans"]. "')";
-		$sonuc=mysqli_query($baglan,$seansekle);
-		if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png" ></div>';
-				header("Refresh: 1; url=seansislemleri.php");
-		}
-			else
-		{
-				echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=seansislemleri.php");
-		}
-	}
-	?>
+<!-----------------------------------------------------------FİLM TÜRÜ İŞLEMLERİ--------------------------------------------------------->
+<?php
+if(isset($_POST["filmturuekle"])) // Eğer "filmturuekle" butonuna tıklanmışsa
+{
+    // Film türü eklemek için SQL sorgusu
+    $filmturekle="insert into filmturleri (film_turu) VALUES ('" . $_POST["filmturu"]. "')";
+    // Sorguyu çalıştır
+    $sonuc=mysqli_query($baglan,$filmturekle);
+    if($sonuc) // Sorgu başarılıysa
+    {
+        echo '<div align="center"><img src="tik.png" ></div>'; // Başarı mesajı
+        header("Refresh: 1; url=filmturu.php"); // 1 saniye sonra film türleri sayfasına yönlendir
+    }
+    else // Sorgu başarısızsa
+    {
+        echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı
+        header("Refresh: 1; url=filmturu.php"); // 1 saniye sonra film türleri sayfasına yönlendir
+    }
+}
+?>
+
+<?php
+if(isset($_POST["filmturusilme"])) // Eğer "filmturusilme" butonuna tıklanmışsa
+{
+    $filmturusil= $_POST['filmturu']; // Silinecek film türü ID'sini al
+    // Film türünü silmek için SQL sorgusu
+    $sorgu="DELETE FROM filmturleri WHERE filmtur_id=$filmturusil";
+    // Sorguyu çalıştır
+    $sonuc=mysqli_query($baglan,$sorgu);
+    if($sonuc) // Sorgu başarılıysa
+    {
+        echo '<div align="center"><img src="tik.png" ></div>'; // Başarı mesajı
+        header("Refresh: 1; url=filmturusil.php"); // 1 saniye sonra film türleri silme sayfasına yönlendir
+    }
+    else // Sorgu başarısızsa
+    {
+        echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı
+        header("Refresh: 1; url=filmturusil.php"); // 1 saniye sonra film türleri silme sayfasına yönlendir
+    }
+}
+?>
+<!------------------------------------------------------------------------------------------------------------------------------------------------>
+<!------------------------------------------------------------------------------------------------------------------------------------------------->
+<!---------------------------------------------------------------SEANS İŞLEMLERİ------------------------------------------------------------->
+<?php
+if(isset($_POST["seansekle"])) // Eğer "seansekle" butonuna tıklanmışsa
+{
+    // Seans eklemek için SQL sorgusu
+    $seansekle="insert into seanslar (film_id,salon_id,seansSaat_id) VALUES ('"   . $_POST["filmid"]. "','" . $_POST["salonid"]."','" . $_POST["seans"]. "')";
+    // Sorguyu çalıştır
+    $sonuc=mysqli_query($baglan,$seansekle);
+    if($sonuc) // Sorgu başarılıysa
+    {
+        echo '<div align="center"><img src="tik.png" ></div>'; // Başarı mesajı
+        header("Refresh: 1; url=seansislemleri.php"); // 1 saniye sonra seans işlemleri sayfasına yönlendir
+    }
+    else // Sorgu başarısızsa
+    {
+        echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı
+        header("Refresh: 1; url=seansislemleri.php"); // 1 saniye sonra seans işlemleri sayfasına yönlendir
+    }
+}
+?>
 
 <!------------------------------------------------------------------------------------------------------------------------------------------------>
 <!------------------------------------------------------------------------------------------------------------------------------------------------->
-<!----------------------------------------------------------------------BİLET İŞLEMLERİ------------------------------------------------------------->
+<!-------------------------------------------------------------BİLET İŞLEMLERİ------------------------------------------------------------->
 <?php
-if(isset($_POST["biletsat"])) 
+if(isset($_POST["biletsat"])) // Eğer "biletsat" butonuna tıklanmışsa
 {
-if($_POST["bilettipi"]=="Öğrenci")
-{
-	$ucret="10TL";
-}
-elseif ($_POST["bilettipi"]=="Tam") 
-{
-	$ucret="15TL";
-}
-$film= $_POST['filmid'];
-$salon= $_POST['salonid'];
-$seansSaat= $_POST['seans_saati'];
-$sorgu="Select*from seanslar WHERE film_id='$film' AND salon_id='$salon' AND seansSaat_id='$seansSaat'";
-$sonuc=mysqli_query($baglan, $sorgu);
-$kayitsayisi=mysqli_num_rows($sonuc);
-if($kayitsayisi>0)
-{
-	$biletsat="insert into biletsatisi (musteri_id,bilet_tipi,film_id,salon_id,koltuk_id,seansSaati_id,tutar) VALUES ('" . $_POST["musteriId"]. "','" . $_POST["bilettipi"]. "','" . $_POST["filmid"]. "','" . $_POST["salonid"]. "','" .$_POST["koltukid"]. "','" . $_POST["seans_saati"]."','"."$ucret". "')";
-	$biletsorgu=mysqli_query($baglan,$biletsat);
-	if($biletsorgu)
-	{
-	echo '<div align="center"><img src="tik.png" ></div>';
-	header("Refresh: 1; url=biletislemleri.php");
-	}
-	else
-	{
-	echo '<div align="center"><img src="hata.png" height="550" ></div>';
-	header("Refresh: 1; url=biletislemleri.php");
-	}
-}
-else
-{
-	echo '<div align="center"><img src="hata.png" height="550" ></div>';
-	header("Refresh: 1; url=biletislemleri.php");
-}
+    if($_POST["bilettipi"]=="Öğrenci") // Eğer bilet türü "Öğrenci" ise
+    {
+        $ucret="10TL"; // Öğrenci bileti ücreti
+    }
+    elseif ($_POST["bilettipi"]=="Tam") // Eğer bilet türü "Tam" ise
+    {
+        $ucret="15TL"; // Tam bileti ücreti
+    }
+    $film= $_POST['filmid']; // Film ID'si
+    $salon= $_POST['salonid']; // Salon ID'si
+    $seansSaat= $_POST['seans_saati']; // Seans saati
+    // Seansı kontrol etmek için SQL sorgusu
+    $sorgu="Select*from seanslar WHERE film_id='$film' AND salon_id='$salon' AND seansSaat_id='$seansSaat'";
+    $sonuc=mysqli_query($baglan, $sorgu);
+    $kayitsayisi=mysqli_num_rows($sonuc); // Kayıt sayısını al
+    if($kayitsayisi>0) // Eğer seans var ise
+    {
+        // Bilet satış işlemi için SQL sorgusu
+        $biletsat="insert into biletsatisi (musteri_id,bilet_tipi,film_id,salon_id,koltuk_id,seansSaati_id,tutar) VALUES ('" . $_POST["musteriId"]. "','" . $_POST["bilettipi"]. "','" . $_POST["filmid"]. "','" . $_POST["salonid"]. "','" .$_POST["koltukid"]. "','" . $_POST["seans_saati"]."','"."$ucret". "')";
+        $biletsorgu=mysqli_query($baglan,$biletsat);
+        if($biletsorgu) // Sorgu başarılıysa
+        {
+            echo '<div align="center"><img src="tik.png" ></div>'; // Başarı mesajı
+            header("Refresh: 1; url=biletislemleri.php"); // 1 saniye sonra bilet işlemleri sayfasına yönlendir
+        }
+        else // Sorgu başarısızsa
+        {
+            echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı
+            header("Refresh: 1; url=biletislemleri.php"); // 1 saniye sonra bilet işlemleri sayfasına yönlendir
+        }
+    }
+    else // Seans yoksa
+    {
+        echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı
+        header("Refresh: 1; url=biletislemleri.php"); // 1 saniye sonra bilet işlemleri sayfasına yönlendir
+    }
 }
 ?>
-
-
-<!------------------------------------------------------------------------------------------------------------------------------------------------>
-<!------------------------------------------------------------------------------------------------------------------------------------------------->
-<!------------------------------------------------------------------------------------------------------------------------------------------------>
-<?php
-if(isset($_POST["giris"]))
-{
-$sorgu=mysqli_query($baglan,"SELECT * FROM personel WHERE personel_kullanici='".$_POST["kullanici_adi"]."' AND sifre='".$_POST["sifre"]."'");
-if($bilgiler=mysqli_fetch_array($sorgu)) 
-{
-	$_SESSION["id"]=$bilgiler["personel_id"];
-    $_SESSION["isim"]=$bilgiler["personel_adi_soyadi"];
-    $_SESSION["kadi"]=$bilgiler["personel_kullanici"];
-    $_SESSION["sifre"]=$bilgiler["sifre"];
-	header("Refresh: 0; url=filmler.php");
-}
-else
-{
-  echo "Kullanıcı adı veya şifre yanlış";
-  header("Refresh: 1; url=index.php");
-}
-}
-?>
-
-<?php
-if(isset($_POST["musterigiris"]))
-{
-$sorgu=mysqli_query($baglan,"SELECT * FROM musteriler WHERE kadi='".$_POST["kullanici_adi"]."' AND sifre='".$_POST["sifre"]."'");
- if ($bilgiler=mysqli_fetch_array($sorgu)) 
- {
-  $_SESSION["id"]=$bilgiler["musteri_id"];
-  $_SESSION["isim"]=$bilgiler["musteri_adi_soyadi"];
-  $_SESSION["kadi"]=$bilgiler["kadi"];
-  $_SESSION["sifre"]=$bilgiler["sifre"];
-  $_SESSION["eposta"]=$bilgiler["eposta"];
-  $_SESSION["telefon"]=$bilgiler["telefon"];
-  $_SESSION["yas"]=$bilgiler["yas"];
-  header("Refresh: 0; url=bilgilerim.php");
-}
-	else
-	{
-		echo "Kullanıcı adı veya şifre yanlış!";
-		header("Refresh: 1; url=musterigiris.php");
-	}
-}
-?>
-<!------------------------------------------------------------------------------------------------------------------------------------------------->
-<?php
-if(isset($_POST["biletal"])) 
-{
-if($_SESSION["yas"]<=24)
-{
-	$ucret="10TL";
-	$bilettipi="Öğrenci";
-}
-else
-{
-	$ucret="15TL";
-	$bilettipi="Tam";
-}
-
-
-$film= $_POST['filmid'];
-$salon= $_POST['salonid'];
-$seansSaat= $_POST['seans_saati'];
-$sorgu="Select*from seanslar WHERE film_id='$film' AND salon_id='$salon' AND seansSaat_id='$seansSaat'";
-$sonuc=mysqli_query($baglan, $sorgu);
-$kayitsayisi=mysqli_num_rows($sonuc);
-if($kayitsayisi>0)
-{
-	$biletsat="insert into biletsatisi (musteri_id,bilet_tipi,film_id,salon_id,koltuk_id,seansSaati_id,tutar) VALUES ('" . $_SESSION["id"]. "','" ."$bilettipi". "','" . $_POST["filmid"]. "','" . $_POST["salonid"]. "','" .$_POST["koltukid"]. "','" . $_POST["seans_saati"]."','"."$ucret". "')";
-	$sonuc=mysqli_query($baglan,$biletsat);
-	if($sonuc)
-	{
-	echo '<div align="center"><img src="tik.png" ></div>';
-	header("Refresh: 1; url=vizyondakifilmler.php");
-	}
-	else
-	{
-	echo '<div align="center"><img src="hata.png" height="550" ></div>';
-	header("Refresh: 1; url=vizyondakifilmler.php");
-	}
-}
-else
-{
-	echo '<div align="center"><img src="hata.png" height="550" ></div>';
-	header("Refresh: 1; url=vizyondakifilmler.php");
-}
-}
-?>
-
-</body>
-</html>
