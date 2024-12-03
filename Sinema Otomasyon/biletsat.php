8ea3207280073ffca1f121cbd65cf2b7e1b0d58d
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:28:31 +0300

    biletsat.php
    Veritabanı Bağlantısı
    baglan.php dosyası ile veritabanı bağlantısı sağlanır ve veri çekme işlemleri yapılır.
    
    Seans Verisi Çekme
    seanslar, seans_saati, filmler, salon tablosundan veriler çekilir. Bu veriler, bilet satış formunda kullanıcıya seçim yapabilmesi için kullanılır.
    
    HTML Formu
    Kullanıcının bilet satışı yapabilmesi için gerekli form elemanları sunulmuş. Bu formda, film, salon, seans saati, müşteri, bilet tipi ve koltuk gibi seçenekler bulunmaktadır. Seçilen verilere göre bilet satışı yapılacaktır.
    
    Dinamik Seçenekler
    Veritabanından alınan filmler, salonlar, seanslar, müşteriler ve koltuklar dinamik olarak formda gösterilmektedir. Her bir dropdown (seçim kutusu) veritabanından çekilen verilere göre otomatik olarak doldurulmaktadır.
    
    Bilet Satışı İşlemi
    Form gönderildiğinde, kullanıcı tarafından seçilen verilere göre bilet satış işlemi yapılacaktır. Form verileri, islemler.php dosyasına gönderilir ve burada işlenir.

--- Sinema Otomasyon/biletsat.php
+++ Sinema Otomasyon/biletsat.php
@@ -1,75 +1,77 @@
-<?php include ("baglan.php");
-$id=@$_GET['id'];
+<?php include ("baglan.php"); // Veritabanı bağlantısı için baglan.php dosyasını dahil ediyoruz
+$id=@$_GET['id']; // Seans ID'sini URL'den alıyoruz
 $tumsorgu="select*from seanslar
 INNER JOIN seans_saati ON seanslar.seansSaat_id=seans_saati.seansSaat_id
 INNER JOIN filmler ON seanslar.film_id=filmler.film_id 
-INNER JOIN salon ON seanslar.salon_id=salon.salon_id where seans_id='$id'";
-$bul=mysqli_query($baglan,$tumsorgu);
-$satir=mysqli_fetch_array($bul); ?>
+INNER JOIN salon ON seanslar.salon_id=salon.salon_id where seans_id='$id'"; // Seans verisini almak için gerekli sorgu
+$bul=mysqli_query($baglan,$tumsorgu); // Sorguyu çalıştırıyoruz
+$satir=mysqli_fetch_array($bul); // Sorgudan dönen verileri alıyoruz
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Sinema Otomasyonu</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width-device-width,intital-scale-1.0">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style.css"> <!-- Sayfa için stil dosyasını dahil ediyoruz -->
 </head>
 <body>
 	<header>
-		<h2><ul>
-		<li><a href="filmler.php">Film İşlemleri</a></li>
-		<li><a href="seansislemleri.php">Seans İşlemleri</a></li>
-		<li><a href="filmturu.php">Film Türü</a></li>
-		<li><a href="biletislemleri.php">Bilet İşlemleri</a></li>
-		<li><a href="musteriler.php">Müşteriler</a></li>
-		<li><a href="cikis.php">Çıkış</a></li>
-		</ul></h2>
+		<h2>
+			<ul>
+				<li><a href="filmler.php">Film İşlemleri</a></li>
+				<li><a href="seansislemleri.php">Seans İşlemleri</a></li>
+				<li><a href="filmturu.php">Film Türü</a></li>
+				<li><a href="biletislemleri.php">Bilet İşlemleri</a></li>
+				<li><a href="musteriler.php">Müşteriler</a></li>
+				<li><a href="cikis.php">Çıkış</a></li>
+			</ul>
+		</h2>
 	</header>
-	<h2>Bilet Satışı</h1>
-		<div class="tableouterb" style="height: 250px">
-		<form action="islemler.php" method="post"><br><br><br><br>
-			<select name="filmid" class="sorgu">
-					<?php
-						echo '<option value="'.$satir[film_id].'">'.$satir[film_adi].'</option>';
-					?>	
-				</select>
-				<select name="salonid" class="sorgu">
-					<?php
-						echo '<option value="'.$satir[salon_id].'">'.$satir[salon_adi].'</option>';
-					?>	
-				</select>
-				<select name="seans_saati" class="sorgu">
-					<?php
-						echo '<option value="'.$satir[seansSaat_id].'">'.$satir[seans_saati].'</option>';
-					?>	
-				</select>
-				<select name="musteriId" class="sorgu"> 
-					<option>Müşteriler</option>
-					<?php
-						$sorgu="select*from musteriler"; 
-						$sql=mysqli_query($baglan,$sorgu); 
-						while($satir=mysqli_fetch_array($sql))
-						{
-						echo '<option value="'.$satir[musteri_id].'">'.$satir[musteri_adi_soyadi].'</option>';
-						}	
-					?>	
-				</select>
-				<select name="bilettipi" class="sorgu">
-					<option>Tam</option>
-					<option>Öğrenci</option>
-				</select>
-				<select name="koltukid" class="sorgu">
-					<option>Koltuklar</option>
-					<?php
-						$sorgu="select*from koltuk";
-						$sonuc=mysqli_query($baglan,$sorgu);
-						while($satir=mysqli_fetch_array($sonuc))
-						{
-						echo '<option value="'.$satir[koltuk_id].'">'.$satir[koltuklar].'</option>';
-						}	
-					?>	
-				</select>
-			<button type="submit" class="sub" id="giris" name="biletsat">Bilet Sat</button>
+	<h2>Bilet Satışı</h2> <!-- Sayfa başlığı -->
+	<div class="tableouterb" style="height: 250px"> <!-- Formu saran dış div -->
+		<form action="islemler.php" method="post"> <!-- Formu başlatıyoruz, işlem için islemler.php'ye veri gönderilecek -->
+			<br><br><br><br>
+			<select name="filmid" class="sorgu"> <!-- Film seçim dropdown'ı -->
+				<?php
+					echo '<option value="'.$satir[film_id].'">'.$satir[film_adi].'</option>'; // Veritabanından seçili filmin adı
+				?>	
+			</select>
+			<select name="salonid" class="sorgu"> <!-- Salon seçim dropdown'ı -->
+				<?php
+					echo '<option value="'.$satir[salon_id].'">'.$satir[salon_adi].'</option>'; // Seçili salon adı
+				?>	
+			</select>
+			<select name="seans_saati" class="sorgu"> <!-- Seans saati dropdown'ı -->
+				<?php
+					echo '<option value="'.$satir[seansSaat_id].'">'.$satir[seans_saati].'</option>'; // Seçili seans saati
+				?>	
+			</select>
+			<select name="musteriId" class="sorgu"> <!-- Müşteri seçimi dropdown'ı -->
+				<option>Müşteriler</option> <!-- Varsayılan seçenek -->
+				<?php
+					$sorgu="select*from musteriler"; // Müşterileri sorguluyoruz
+					$sql=mysqli_query($baglan,$sorgu); // Sorguyu çalıştırıyoruz
+					while($satir=mysqli_fetch_array($sql)) { // Her müşteri için döngü
+						echo '<option value="'.$satir[musteri_id].'">'.$satir[musteri_adi_soyadi].'</option>'; // Müşteri ismi ekleniyor
+					}	
+				?>	
+			</select>
+			<select name="bilettipi" class="sorgu"> <!-- Bilet tipi seçimi dropdown'ı -->
+				<option>Tam</option> <!-- Tam bilet seçeneği -->
+				<option>Öğrenci</option> <!-- Öğrenci bilet seçeneği -->
+			</select>
+			<select name="koltukid" class="sorgu"> <!-- Koltuk seçimi dropdown'ı -->
+				<option>Koltuklar</option> <!-- Varsayılan seçenek -->
+				<?php
+					$sorgu="select*from koltuk"; // Koltukları sorguluyoruz
+					$sonuc=mysqli_query($baglan,$sorgu); // Koltukları veritabanından alıyoruz
+					while($satir=mysqli_fetch_array($sonuc)) { // Her koltuk için döngü
+						echo '<option value="'.$satir[koltuk_id].'">'.$satir[koltuklar].'</option>'; // Koltuk adı ekleniyor
+					}	
+				?>	
+			</select>
+			<button type="submit" class="sub" id="giris" name="biletsat">Bilet Sat</button> <!-- Bilet satma butonu -->
 		</form>
 	</div>
 </body>
