6b7798f6ce42aeef66fc2ec1a5b7a9a6cb0b9445
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:37:22 +0300

    filmguncelle.php
    Veritabanı Bağlantısı:
    
    include("baglan.php"); komutu, veritabanına bağlanmak için gerekli olan PHP dosyasını dahil eder.
    Film Seçme ve Sorgulama:
    
    $_GET['id'] ile gelen film ID'sini alır ve bu ID'ye göre veritabanında sorgu yapar ($tumsorgu).
    mysqli_query fonksiyonu, sorguyu çalıştırarak sonuçları döndürür. mysqli_fetch_array ile bu sonuç, dizi şeklinde alınır ve film bilgileri ekrana yerleştirilir.
    Form Gönderimi:
    
    Film adı ve türü gibi bilgilerin güncellenebilmesi için bir form oluşturulur. Kullanıcı, film adı ve türünü düzenleyebilir.
    Film türleri, filmturleri tablosundan çekilir ve her biri bir <option> olarak formda gösterilir.
    Kullanıcı bilgileri girdikten sonra, filmguncelle butonuna tıklayarak formu gönderir.
    Film Güncelleme İşlemi:
    
    Form verileri POST yöntemiyle alındığında, $_POST['filmadi'] ve $_POST['filmturu'] ile kullanıcıdan alınan yeni bilgilerle veritabanındaki film kaydı güncellenir.
    Eğer güncelleme başarılıysa, kullanıcıya "Güncellendi" mesajı gösterilir. Aksi takdirde "Güncelleme Başarısız." mesajı görüntülenir.
    Geri Dönme Butonu:
    
    Kullanıcı, bir hata durumunda veya işlem bittikten sonra filmler.php sayfasına geri dönebilir.
    Bu Kodun Amacı:
    Bu PHP kodu, kullanıcıların mevcut bir filmi güncelleyebilmelerini sağlamak için kullanılır. Film adı ve türü gibi bilgilerin düzenlenmesi ve güncellenmesi işlemi yapılır.

--- Sinema Otomasyon/filmguncelle.php
+++ Sinema Otomasyon/filmguncelle.php
@@ -1,15 +1,18 @@
-<?php include("baglan.php");
-$id=@$_GET['id'];
-$tumsorgu="select*from filmler where film_id=$id";
-$bul=mysqli_query($baglan,$tumsorgu);
-$satir=mysqli_fetch_array($bul);?>
+<?php include("baglan.php"); // Veritabanı bağlantısını içeri aktarır.
+$id=@$_GET['id']; // 'id' parametresini GET yöntemi ile alır. Bu, güncellenmek istenen filmin ID'sidir.
+
+$tumsorgu="select*from filmler where film_id=$id"; // Seçilen film ID'sine göre 'filmler' tablosundan film bilgilerini sorgular.
+$bul=mysqli_query($baglan,$tumsorgu); // SQL sorgusunu çalıştırır ve sonucu $bul değişkenine atar.
+$satir=mysqli_fetch_array($bul); // Sorgu sonucunu bir dizi olarak alır.
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Sinema Otomasyonu</title>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width-device-width,intital-scale-1.0">
-	<link rel="stylesheet" href="css/style.css">
+	<meta name="viewport" content="width=device-width,intital-scale=1.0">
+	<link rel="stylesheet" href="css/style.css"> <!-- Stil dosyasını bağlar. -->
 </head>
 <body>
 	<header>
@@ -22,39 +25,45 @@ $satir=mysqli_fetch_array($bul);?>
 		<li><a href="cikis.php">Çıkış</a></li>
 		</ul></h2>
 	</header>
-	<h2>Film Güncelleme</h1>
+	<h2>Film Güncelleme</h2>
 		<div class="tableouterf" style="height: 400px"><br><br><br>
 		<form action="" method="post">
-			<input type="text" name="filmadi" placeholder="Film Adı" required="" value="<?php echo $satir['film_adi'];?>"><br>			
+			<input type="text" name="filmadi" placeholder="Film Adı" required="" value="<?php echo $satir['film_adi'];?>"><br> <!-- Film adı prefilled olarak gelir. -->
+			
 			<select name="filmturu" class="sorgu">
 				<option>Film Türü</option>
 				<?php
-					$sorgu="select*from filmturleri";
-					$sonuc=mysqli_query($baglan,$sorgu);
-					while($satir=mysqli_fetch_array($sonuc))
+					$sorgu="select*from filmturleri"; // Film türlerini almak için sorgu çalıştırılır.
+					$sonuc=mysqli_query($baglan,$sorgu); // Sorgu çalıştırılır ve sonuç alınır.
+					while($satir=mysqli_fetch_array($sonuc)) // Her film türü için bir seçenek oluşturulur.
 					{
 					echo '<option value="'.$satir[filmtur_id].'">'.$satir[film_turu].'</option>';
 					}	
 				?>	
 			</select><br>
-			<button class="sub" type="submit" id="giris" name="filmguncelle">Filmi Güncelle</button>		
+			<button class="sub" type="submit" id="giris" name="filmguncelle">Filmi Güncelle</button> <!-- Form gönderme butonu -->
 		</form>
-		<a href="filmler.php"><button type="submit" class="sub" id="uye" name="uye">Geri Çık</button></a><br>
+		<a href="filmler.php"><button type="submit" class="sub" id="uye" name="uye">Geri Çık</button></a><br> <!-- Geri dönme butonu -->
 	</div><br>	
 </body>
 </html>
 
 <?php
+// Form gönderildiyse, yani 'filmguncelle' butonuna tıklanmışsa:
 if(isset($_POST["filmguncelle"]))
 {
+	// Film bilgilerini güncelleme sorgusu
 	$guncelle="update filmler set film_adi='".$_POST['filmadi']."',filmtur_id='". $_POST['filmturu']."'where film_id='$id'";
+
+	// Sorguyu çalıştırır
 	$sonuc=mysqli_query($baglan,$guncelle);
+	// Etkilenen satır sayısını kontrol eder
 	$etkilenensatir=mysqli_affected_rows($baglan);
-	if($etkilenensatir>0)
+	if($etkilenensatir>0) // Eğer etkilenen satır varsa, güncelleme başarılı
 		{
 				echo 'Güncellendi';
 		}
-			else
+			else // Güncelleme başarısız olduysa
 		{
 			echo 'Güncelleme Başarısız.';
 		}
