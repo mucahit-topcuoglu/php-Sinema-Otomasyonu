c77273de682aa58b6569e17c090b65ad022c3f00
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:30:41 +0300

    biletsil.php
    Veritabanı Bağlantısı
    baglan.php dosyası ile veritabanı bağlantısı sağlanır.
    
    Bilet Silme İşlemi
    URL'den alınan id parametresi kullanılarak, biletsatisi tablosunda belirtilen bilet_id'ye sahip kayıt silinir. Bu işlem için SQL DELETE komutu kullanılır.
    
    Sonuç Kontrolü
    Sorgu başarılı olursa, onay olarak bir tik.png görseli gösterilir ve kullanıcı biletislemleri.php sayfasına yönlendirilir. Eğer sorgu başarısız olursa, hata olarak hata.png görseli gösterilir ve yine kullanıcı aynı sayfaya yönlendirilir.
    
    Yönlendirme
    Hem başarılı hem de başarısız işlemler sonrası, 1 saniye bekledikten sonra kullanıcı otomatik olarak biletislemleri.php sayfasına yönlendirilir.

--- Sinema Otomasyon/biletsil.php
+++ Sinema Otomasyon/biletsil.php
@@ -1,16 +1,17 @@
 <?php
-include "baglan.php";
-	$biletsilme=@$_GET['id'];
-		$sorgu="DELETE FROM biletsatisi WHERE bilet_id=$biletsilme";
-		$sonuc=mysqli_query($baglan,$sorgu);
-			if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png"></div>';
-				header("Refresh: 1; url=biletislemleri.php");
-		}
-			else
-		{
-				echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=biletislemleri.php");
-		}
+include "baglan.php"; // Veritabanı bağlantısı için baglan.php dosyasını dahil ediyoruz
+
+$biletsilme=@$_GET['id']; // URL üzerinden gönderilen 'id' parametresini alıyoruz, bu ID biletin ID'sidir
+
+$sorgu="DELETE FROM biletsatisi WHERE bilet_id=$biletsilme"; // 'bilet_id' ile eşleşen bileti silmek için SQL sorgusunu oluşturuyoruz
+
+$sonuc=mysqli_query($baglan,$sorgu); // Sorguyu veritabanında çalıştırıyoruz
+
+if($sonuc) { // Eğer sorgu başarılı olduysa
+    echo '<div align="center"><img src="tik.png"></div>'; // Başarı mesajı olarak bir onay resmi gösteriyoruz
+    header("Refresh: 1; url=biletislemleri.php"); // 1 saniye sonra bilet işlemleri sayfasına yönlendiriyoruz
+} else { // Eğer sorgu başarısız olduysa
+    echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı olarak bir hata resmi gösteriyoruz
+    header("Refresh: 1; url=biletislemleri.php"); // 1 saniye sonra bilet işlemleri sayfasına yönlendiriyoruz
+}
 ?>
