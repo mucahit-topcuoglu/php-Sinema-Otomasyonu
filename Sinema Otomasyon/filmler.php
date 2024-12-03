71759b81d6de5d242084f2b6da70e19598c39e3d
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:38:45 +0300

    filmler.php
    Veritabanı Bağlantısı:
    
    include "baglan.php"; komutu, veritabanı bağlantısını sağlayan PHP dosyasını dahil eder.
    Film Ekleme Formu:
    
    Film adı ve türünü girerek yeni bir film eklemek için form oluşturulmuştur.
    Film türleri filmturleri tablosundan çekilerek <select> içinde seçenek olarak sunulmaktadır.
    Film Görüntüleme:
    
    "Filmleri Görüntüle" butonuna tıklandığında, veritabanındaki filmler tablosu ile filmturleri tablosu birleştirilerek filmler ve türleri görüntülenir.
    INNER JOIN kullanılarak filmturleri tablosundaki film türleri ile filmler tablosundaki filmler birleştirilir.
    Her film için güncelleme ve silme işlemleri yapılabilmesi için uygun bağlantılar (butonlar) sunulur.
    Film Güncelleme ve Silme:
    
    Güncelleme için her film için bir Filmi Güncelle butonu eklenmiş, bu butonla filmguncelle.php sayfasına yönlendirilir.
    Silme için her film için bir Filmi Sil butonu eklenmiş, bu butonla filmsil.php sayfasına yönlendirilir.
    Özellikler:
    Film Ekleme: Yeni bir film adı ve türü girilebilir.
    Film Görüntüleme: Filmler ve türleri tablo şeklinde görüntülenebilir.
    Film Güncelleme: Filmi Güncelle butonuna tıklanarak film bilgileri güncellenebilir.
    Film Silme: Filmi Sil butonuna tıklanarak film silinebilir.
    Bu PHP kodu, sinema otomasyonu sistemi için film yönetimi işlemlerini basit bir şekilde gerçekleştirmenizi sağlar.

--- Sinema Otomasyon/filmler.php
+++ Sinema Otomasyon/filmler.php
@@ -1,11 +1,11 @@
-<?php include "baglan.php";?>
+<?php include "baglan.php"; ?> <!-- Veritabanı bağlantısını dahil eder. -->
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Sinema Otomasyonu</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width-device-width,intital-scale-1.0">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style.css"> <!-- CSS dosyasını bağlar. -->
 </head>
 <body>
 	<header>
@@ -18,29 +18,32 @@
 		<li><a href="cikis.php">Çıkış</a></li>
 		</ul></h2>
 	</header>
-	<h2>Film İşlemleri</h1>
-		<div class="tableouterf" style="height: 400px"><br><br><br>
+	<h2>Film İşlemleri</h2> <!-- Başlık -->
+	<div class="tableouterf" style="height: 400px"><br><br><br>
+		<!-- Film eklemek için form -->
 		<form action="islemler.php" method="post">
-			<input type="text" name="filmadi" placeholder="Film Adı" required=""><br>			
+			<input type="text" name="filmadi" placeholder="Film Adı" required=""><br> <!-- Film adı girişi -->
 			<select name="filmturu" class="sorgu">
-				<option>Film Türü</option>
+				<option>Film Türü</option> <!-- Film türü seçimi -->
 				<?php
-					$sorgu="select*from filmturleri";
-					$sonuc=mysqli_query($baglan,$sorgu);
-					while($satir=mysqli_fetch_array($sonuc))
+					$sorgu="select*from filmturleri"; // Film türlerini çekme sorgusu
+					$sonuc=mysqli_query($baglan,$sorgu); // Sorguyu çalıştır
+					while($satir=mysqli_fetch_array($sonuc)) // Her film türünü seçenek olarak ekler
 					{
-					echo '<option value="'.$satir[filmtur_id].'">'.$satir[film_turu].'</option>';
+						echo '<option value="'.$satir['filmtur_id'].'">'.$satir['film_turu'].'</option>';
 					}	
 				?>	
 			</select><br>
-			<button class="sub" type="submit" id="giris" name="filmekle">Film Ekle</button>
+			<button class="sub" type="submit" id="giris" name="filmekle">Film Ekle</button> <!-- Film ekleme butonu -->
 		</form>
+
+		<!-- Filmleri görüntülemek için form -->
 		<form action="" method="post">
-		<button class="sub" type="submit" id="giris" name="filmgoruntule">Filmleri Görüntüle</button>
-		</form>			
+			<button class="sub" type="submit" id="giris" name="filmgoruntule">Filmleri Görüntüle</button> <!-- Filmleri görüntüle butonu -->
+		</form>	
+
 <?php
-if(isset($_POST["filmgoruntule"]))
-{	
+if(isset($_POST["filmgoruntule"])) {	// Eğer "Filmleri Görüntüle" butonuna tıklanırsa
 ?>
 <table id="personel">
 <tr>
@@ -51,17 +54,16 @@ if(isset($_POST["filmgoruntule"]))
 	<th>Filmi Sil</th>
 </tr>
 <?php
-$listele="select film_id, film_adi, film_turu from filmler INNER JOIN filmturleri ON filmler.filmtur_id=filmturleri.filmtur_id";
-$sorgu=mysqli_query($baglan,$listele);
-while($satir=mysqli_fetch_array($sorgu))
-{
+$listele="select film_id, film_adi, film_turu from filmler INNER JOIN filmturleri ON filmler.filmtur_id=filmturleri.filmtur_id"; // Filmleri ve türlerini listelemek için sorgu
+$sorgu=mysqli_query($baglan,$listele); // Sorguyu çalıştır
+while($satir=mysqli_fetch_array($sorgu)) { // Her film için bir satır oluştur
 ?>
 <tr>
-<td><?php echo $satir["film_id"]?></td>
-<td><?php echo $satir["film_adi"]?></td>
-<td><?php echo $satir["film_turu"]?></td>
-<td><a href="filmguncelle.php?id=<?php echo $satir['film_id'];?>"><button type="submit" class="sub1" id="giris" name="uye">Filmi Güncelle</button></a></td>
-<td><a href="filmsil.php?id=<?php echo $satir['film_id'];?>"><button type="submit" class="sub1" id="uye" name="uye">Filmi Sil</button></a></td>
+<td><?php echo $satir["film_id"];?></td> <!-- Film ID'si -->
+<td><?php echo $satir["film_adi"];?></td> <!-- Film adı -->
+<td><?php echo $satir["film_turu"];?></td> <!-- Film türü -->
+<td><a href="filmguncelle.php?id=<?php echo $satir['film_id'];?>"><button type="submit" class="sub1" id="giris" name="uye">Filmi Güncelle</button></a></td> <!-- Film güncelleme butonu -->
+<td><a href="filmsil.php?id=<?php echo $satir['film_id'];?>"><button type="submit" class="sub1" id="uye" name="uye">Filmi Sil</button></a></td> <!-- Film silme butonu -->
 </tr>
 <?php
 }
@@ -71,4 +73,3 @@ while($satir=mysqli_fetch_array($sorgu))
 	</div><br>	
 </body>
 </html>
-
