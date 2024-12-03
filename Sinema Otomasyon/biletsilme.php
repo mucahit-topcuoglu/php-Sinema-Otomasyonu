2ead3d9fd71e12da94a01939967b6b1900d5d874
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:32:25 +0300

    biletsilme.php
    Veritabanı Bağlantısı
    baglan.php dosyası, veritabanı bağlantısını sağlar. Bu dosya, veritabanı sunucusu, kullanıcı adı, şifre ve veritabanı adı gibi bilgileri içerir.
    
    Bilet Silme
    URL'den alınan id parametresi kullanılarak, biletsatisi tablosunda belirtilen bilet_id'ye sahip kayıt silinir. SQL DELETE komutu bu işlemi gerçekleştirir.
    
    Başarı Durumu
    Eğer SQL sorgusu başarıyla çalıştırılmışsa, kullanıcıya bir başarı görseli (tik.png) gösterilir ve 1 saniye içinde kullanıcı biletlerim.php sayfasına yönlendirilir.
    
    Başarısızlık Durumu
    Eğer sorgu başarısız olursa, hata görseli (hata.png) gösterilir ve yine kullanıcı aynı sayfaya yönlendirilir.

--- Sinema Otomasyon/biletsilme.php
+++ Sinema Otomasyon/biletsilme.php
@@ -1,16 +1,17 @@
 <?php
-include "baglan.php";
-	$biletsilme=@$_GET['id'];
-		$sorgu="DELETE FROM biletsatisi WHERE bilet_id=$biletsilme";
-		$sonuc=mysqli_query($baglan,$sorgu);
-			if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png"></div>';
-				header("Refresh: 1; url=biletlerim.php");
-		}
-			else
-		{
-				echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=biletlerim.php");
-		}
+include "baglan.php"; // Veritabanı bağlantısını sağlamak için baglan.php dosyasını dahil ediyoruz
+
+$biletsilme=@$_GET['id']; // URL üzerinden gelen 'id' parametresini alıyoruz. Bu parametre biletin benzersiz ID'sidir.
+
+$sorgu="DELETE FROM biletsatisi WHERE bilet_id=$biletsilme"; // 'bilet_id' ile eşleşen kaydı silmek için SQL sorgusunu oluşturuyoruz.
+
+$sonuc=mysqli_query($baglan,$sorgu); // Veritabanına sorguyu gönderip işlemi başlatıyoruz.
+
+if($sonuc) { // Eğer sorgu başarılı olduysa
+    echo '<div align="center"><img src="tik.png"></div>'; // Başarı mesajı olarak bir onay görseli (tik.png) gösteriyoruz
+    header("Refresh: 1; url=biletlerim.php"); // 1 saniye sonra 'biletlerim.php' sayfasına yönlendiriyoruz
+} else { // Eğer sorgu başarısız olursa
+    echo '<div align="center"><img src="hata.png" height="550" ></div>'; // Hata mesajı olarak bir hata görseli (hata.png) gösteriyoruz
+    header("Refresh: 1; url=biletlerim.php"); // 1 saniye sonra 'biletlerim.php' sayfasına yönlendiriyoruz
+}
 ?>
