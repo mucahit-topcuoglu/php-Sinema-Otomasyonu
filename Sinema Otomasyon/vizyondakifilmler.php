766afd3a9f657f81d51af4fdf950adc7b53fc37a
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 20:07:34 +0300

    vizyondakifilmler.php
    Veritabanı Bağlantısı:
    baglan.php dosyası ile veritabanı bağlantısı sağlanır.
    
    HTML Yapısı:
    Sayfa başlığı ve menü elemanları oluşturulur. Menüde kullanıcı profil, biletler, seanslar ve çıkış gibi sayfalara yönlendiren bağlantılar bulunur.
    
    Seanslar Tablosu:
    seanslar, seans_saati, filmler, ve salon tablolarından veriler INNER JOIN ile birleştirilerek sunulur. Tablo, her seansın ID'si, film adı, salon adı ve seans saati gibi bilgileri içerir.
    
    Bilet Al Butonu:
    Her bir seansın yanında yer alan "Bilet Al" butonuna tıklanarak kullanıcı, o seansın ID'siyle biletal.php sayfasına yönlendirilir.
    
    Güvenlik:
    Bu kodda veritabanına doğrudan kullanıcıdan alınan verilerle işlem yapılmaktadır. Parametreli sorgular kullanılarak güvenlik artırılabilir.

--- Sinema Otomasyon/vizyondakifilmler.php
+++ Sinema Otomasyon/vizyondakifilmler.php
@@ -1,46 +1,47 @@
-<?php include ("baglan.php");?>
+<?php include ("baglan.php");?> <!-- Veritabanı bağlantısını sağlamak için 'baglan.php' dosyasını dahil eder -->
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Sinema Otomasyonu</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width-device-width,intital-scale-1.0">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style.css"> <!-- Sayfa stil dosyasını dahil eder -->
 </head>
 <body>
 	<header>
 		<h2><ul>
-		<li><a href="bilgilerim.php">Profilim</a></li>
-		<li><a href="Biletlerim.php">Biletlerim</a></li>
-		<li><a href="vizyondakifilmler.php">Seanslar</a></li>
-		<li><a href="cikis.php">Çıkış</a></li>
+		<li><a href="bilgilerim.php">Profilim</a></li> <!-- Kullanıcı profil sayfasına yönlendiren link -->
+		<li><a href="Biletlerim.php">Biletlerim</a></li> <!-- Kullanıcının almış olduğu biletlerin listeleneceği sayfa -->
+		<li><a href="vizyondakifilmler.php">Seanslar</a></li> <!-- Seansların listeleneceği sayfa -->
+		<li><a href="cikis.php">Çıkış</a></li> <!-- Kullanıcı çıkışı yapacak -->
 		</ul></h2>
 	</header>
-	<h2>Seanslar</h1>
+	<h2>Seanslar</h2> <!-- Sayfa başlığı -->
 		<div class="tableouterbg">
 				<div align="center">
 				<table id="personel">
 				<tr>
-				<th>Seans id</th>
-				<th>Film Adı</th>
-				<th>Salon Adı</th>
-				<th>Seans Saati</th>
-				<th>Bileti Seç</th>
+				<th>Seans id</th> <!-- Seans ID'si -->
+				<th>Film Adı</th> <!-- Film adı -->
+				<th>Salon Adı</th> <!-- Salon adı -->
+				<th>Seans Saati</th> <!-- Seans saati -->
+				<th>Bileti Seç</th> <!-- Bilet almak için buton -->
 				</tr>
 				<?php
+				// Veritabanından seans bilgilerini almak için sorgu yazılır
 				$sorgu=mysqli_query($baglan,"select*from seanslar
 	 			INNER JOIN seans_saati ON seanslar.seansSaat_id=seans_saati.seansSaat_id
 	 			INNER JOIN filmler ON seanslar.film_id=filmler.film_id 
 				INNER JOIN salon ON seanslar.salon_id=salon.salon_id");
-				while($satir=mysqli_fetch_array($sorgu))
+				while($satir=mysqli_fetch_array($sorgu)) // Her bir satır için döngü başlatılır
 				{
 				?>
 				<tr>
-				<td><?php echo $satir["seans_id"]?></td>	
-				<td><?php echo $satir["film_adi"]?></td>
-				<td><?php echo $satir["salon_adi"]?></td>
-				<td><?php echo $satir["seans_saati"]?></td>
-				<td><a href="biletal.php?id=<?php echo $satir['seans_id'];?>"><button type="submit" class="sub1" id="giris" name="">Bilet Al</button></a></td>
+				<td><?php echo $satir["seans_id"]?></td> <!-- Seans ID'si -->
+				<td><?php echo $satir["film_adi"]?></td> <!-- Film adı -->
+				<td><?php echo $satir["salon_adi"]?></td> <!-- Salon adı -->
+				<td><?php echo $satir["seans_saati"]?></td> <!-- Seans saati -->
+				<td><a href="biletal.php?id=<?php echo $satir['seans_id'];?>"><button type="submit" class="sub1" id="giris" name="">Bilet Al</button></a></td> <!-- Bilet al butonu, kullanıcıyı biletal.php sayfasına yönlendirir -->
 				<tr>
 				<?php
 				}
