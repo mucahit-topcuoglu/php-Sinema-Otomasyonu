e54b23991d0b54266bd730e1e0267eb29a472937
Mücahit <98738306+mucahit-topcuoglu@users.noreply.github.com>
2024-12-03 19:59:56 +0300

    musterisil.php
    Veritabanı Bağlantısı: include "baglan.php"; komutu ile baglan.php dosyasındaki veritabanı bağlantısı dahil edilir.
    Müşteri ID'si Alma: $_GET['id'] parametresi URL üzerinden alınır. Bu parametre, hangi müşterinin silineceğini belirler.
    Silme Sorgusu: $sorgu değişkeni, DELETE SQL sorgusunu içerir. Bu sorgu, musteriler tablosunda, musteri_id değeri ile eşleşen satırı siler.
    Sorgu Çalıştırma: mysqli_query() fonksiyonu, yukarıdaki sorguyu çalıştırır ve işlemin başarılı olup olmadığını kontrol eder.
    Başarı Durumu: Eğer silme işlemi başarılı olduysa, başarılı simgeyi (tik işareti) ve ardından 1 saniye sonra musteriler.php sayfasına yönlendiren bir başlık (header) gönderilir.
    Hata Durumu: Eğer silme işlemi başarısız olduysa, hata simgesini gösterir ve yine aynı şekilde 1 saniye sonra kullanıcıyı musteriler.php sayfasına yönlendirir.

--- Sinema Otomasyon/musterisil.php
+++ Sinema Otomasyon/musterisil.php
@@ -1,16 +1,20 @@
 <?php
-include "baglan.php";
-	$musterisilme=@$_GET['id'];
-		$sorgu="DELETE FROM musteriler WHERE musteri_id=$musterisilme";
-		$sonuc=mysqli_query($baglan,$sorgu);
-			if($sonuc)
-		{
-				echo '<div align="center"><img src="tik.png" ></div>';
-				header("Refresh: 1; url=musteriler.php");
-		}
-			else
-		{
-				echo '<div align="center"><img src="hata.png" height="550" ></div>';
-				header("Refresh: 1; url=musteriler.php");
-		}
+include "baglan.php";  // Veritabanı bağlantısı için baglan.php dosyasını dahil eder.
+
+$musterisilme = @$_GET['id'];  // URL üzerinden gelen 'id' parametresini alır. Bu, silinecek müşterinin ID'sidir. '@' sembolü, hata mesajlarını gizler.
+
+$sorgu = "DELETE FROM musteriler WHERE musteri_id=$musterisilme";  // Veritabanı sorgusu, belirtilen 'musteri_id' ile eşleşen müşteri kaydını siler.
+
+$sonuc = mysqli_query($baglan, $sorgu);  // Sorguyu çalıştırır ve sonucu $sonuc değişkenine atar.
+
+if($sonuc)  // Eğer sorgu başarılı ise (yani bir müşteri silinirse)
+{
+    echo '<div align="center"><img src="tik.png" ></div>';  // Başarı mesajı olarak, "tik.png" (onay simgesi) gösterir.
+    header("Refresh: 1; url=musteriler.php");  // 1 saniye sonra müşteri listesi sayfasına (musteriler.php) yönlendirir.
+}
+else  // Eğer sorgu başarısız ise
+{
+    echo '<div align="center"><img src="hata.png" height="550" ></div>';  // Hata mesajı olarak, "hata.png" (hata simgesi) gösterir.
+    header("Refresh: 1; url=musteriler.php");  // 1 saniye sonra müşteri listesi sayfasına (musteriler.php) yönlendirir.
+}
 ?>
